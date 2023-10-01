2d47ff5e98fb2f3560493fb4fdcc3b37eba938ee
Mithilesh Sharma <mithileshsharma539@gmail.com>
2023-10-01 20:07:24 +0530

    added msg to dms

--- src/db/index.js
+++ src/db/index.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 const uri = process.env.DB_URI;
-mongoose
-  .connect(uri)
-  .then(() => {
-    console.log("Connected to db............");
-  })
-  .catch((e) => {
-    console.log("error", e);
-  });
+
+module.exports = async function getDb() {
+  return mongoose
+    .connect(uri)
+    .then(() => {
+      console.log("Connected to db............");
+    })
+    .catch((e) => {
+      console.log("error", e);
+    });
+};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-require("./db")
+const getDb = require("./db");
 const { Client, IntentsBitField } = require("discord.js");
 const client = new Client({
   intents: [
@@ -9,24 +9,34 @@ const client = new Client({
     IntentsBitField.Flags.GuildMessages,
   ],
 });
+getDb().then(() => {
+  client.on("ready", (client) => {
+    console.log(`${client.user.username} is now awaken !`);
+  });
 
-client.on("ready", (client) => {
-  console.log(`${client.user.username} is now awaken !`);
-});
-
-client.on("messageCreate",(e)=>{
-    if(e.author.bot) return;
-    if(e.content==="Hello"){
-        e.reply("Hello");
+  client.on("messageCreate", (e) => {
+    if (e.author.bot) return;
+    if (e.content === "Hello") {
+      e.reply("Hello");
     }
-})
+  });
+  client.on("messageCreate", (e) => {
+    if (e.author.bot) return;
+    if (e.content === "Mellow") {
+      client.users.cache.get(e.author.id).send("Hehe boi Got em!!!!");
+    }
+  });
 
-client.on("interactionCreate", async (interaction) => {
-  if (!interaction.isChatInputCommand()) return;
+  client.on("interactionCreate", async (interaction) => {
+    if (!interaction.isChatInputCommand()) return;
 
-  if (interaction.commandName === "ping") {
-    await interaction.reply("Pong!");
-  }
+    if (interaction.commandName === "ping") {
+      await interaction.reply({
+        content: `Hello ${interaction.user.globalName} , You have now successfully registered to this event.`,
+        ephemeral: true,
+      });
+    }
+  });
+
+  client.login(process.env.TOKEN);
 });
-
-client.login(process.env.TOKEN);
